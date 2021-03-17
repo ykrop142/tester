1963dfa9eaa7e18df5352695b145053a18ae0648
ykrop142 <ykrop142@gmail.com>
2021-03-17 12:49:51 +0700

    create user table

--- database/migrations/2021_03_10_053036_user_table.php
+++ database/migrations/2021_03_10_053036_user_table.php
@@ -15,12 +15,10 @@ class UserTable extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('fam');
             $table->string('email');
-            $table->string('password');
-            $table->string('avatar')->default('https://img2.freepng.ru/20180521/ocp/kisspng-computer-icons-user-profile-avatar-french-people-5b0365e4f1ce65.9760504415269493489905.jpg');
-            $table->timestamps();
+            $table->string('password')->default('test');
+            $table->integer('group_id')->default(0);
+            $table->integer('permission_id');
         });
     }
 
