841918135ccd32497151c8d2290d697f2bcf09a8
ykrop142 <ykrop142@gmail.com>
2021-03-17 12:50:08 +0700

    create data users table

---
+++ database/migrations/2021_03_17_043253_user_data_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('fam');
+            $table->string('phon_numb');
+            $table->string('avatar')->default('https://img2.freepng.ru/20180521/ocp/kisspng-computer-icons-user-profile-avatar-french-people-5b0365e4f1ce65.9760504415269493489905.jpg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
